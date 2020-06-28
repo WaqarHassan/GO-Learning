df206d5e76099b0d1bc551f55d3432f00ef8beba
WaqarHassan <this.waqar@gmail.com>
2020-06-28 21:44:23 +0500

    changes

--- var_print.go
+++ var_print.go
@@ -10,11 +10,20 @@ func main() {
 		section float32 = 12
 	)
 
+	var isMale bool = true
+
 	var without = 10
 	fmt.Println("Name = ", name)
 	fmt.Println("Section = ", section)
 	fmt.Println("Age = ", age)
+	fmt.Println("Is Male = ", isMale)
 	fmt.Println("Specifying data type is optional = ", without)
-	fmt.Println("User Details = ", name, age, section)
+	fmt.Println("User Details = ", name, age, "\nSection: ", section)
+
+	const PI = 3.14159
+	fmt.Println("Value of PI = ", PI)
+
+	fmt.Println("PI = %i \n", PI)
+	fmt.Printf("%0.2f", PI)
 
 }
